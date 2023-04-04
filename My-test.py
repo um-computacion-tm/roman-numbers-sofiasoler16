dcda0348ae1099b69ef3ac7e7582ae52c3f05ca1
sofiasoler16 <s.soler@alumno.um.edu.ar>
2023-04-04 20:48:28 -0300

    numerosromanos

--- My-test.py
+++ My-test.py
@@ -4,6 +4,8 @@ from my_romanos import entero
 from my_romanos import unidadestorom
 from my_romanos import dectorom
 from my_romanos import centtorom
+from my_romanos import romantodecimal
+#TEST DE DECIMAL A ROMANO
 
 class TestDecimalToRoman(unittest.TestCase):
     def test_partedeunidades (self):
@@ -36,8 +38,67 @@ class TestDecimalToRoman(unittest.TestCase):
     def test_entero999 (self):
         resultado = entero("999")
         self.assertEqual(resultado, "CMXCIX")
-    
-    
+
+
+#TEST DE ROMANO A DECIMAL
+
+    def test_r1(self):
+        resp= romantodecimal("I")
+        self.assertEqual(resp, 1)
+    def test_r4(self):
+        resp= romantodecimal("IV")
+        self.assertEqual(resp, 4)
+    def test_r5(self):
+        resp= romantodecimal("V")
+        self.assertEqual(resp, 5)
+    def test_r9(self):
+        resp= romantodecimal("IX")
+        self.assertEqual(resp, 9)
+    def test_r15(self):
+        resp= romantodecimal("XV")
+        self.assertEqual(resp, 15)
+    def test_r34(self):
+        resp= romantodecimal("XXXIV")
+        self.assertEqual(resp, 34)
+    def test_r90(self):
+        resp= romantodecimal("XC")
+        self.assertEqual(resp, 90)
+    def test_r106(self):
+        resp= romantodecimal("CVI")
+        self.assertEqual(resp, 106)
+    def test_r129(self):
+        resp= romantodecimal("CXXIX")
+        self.assertEqual(resp, 129)
+    def test_r180(self):
+        resp= romantodecimal("CLXXX")
+        self.assertEqual(resp, 180)
+    def test_r182(self):
+        resp= romantodecimal("CLXXXII")
+        self.assertEqual(resp, 182)
+    def test_r190(self):
+        resp= romantodecimal("CXC")
+        self.assertEqual(resp, 190)
+    def test_r390(self):
+        resp= romantodecimal("CCCXC")
+        self.assertEqual(resp, 390)
+    def test_r499(self):
+        resp= romantodecimal("CDXCIX")
+        self.assertEqual(resp, 499)
+    def test_r579(self):
+        resp= romantodecimal("DLXXIX")
+        self.assertEqual(resp, 579)
+    def test_r750(self):
+        resp= romantodecimal("DCCL")
+        self.assertEqual(resp, 750)
+    def test_r900(self):
+        resp= romantodecimal("CM")
+        self.assertEqual(resp, 900)
+    def test_r999(self):
+        resp= romantodecimal("CMXCIX")
+        self.assertEqual(resp, 999)
+    def test_r1352(self):
+        resp= romantodecimal("MCCCLII")
+        self.assertEqual(resp, 1352)
 if __name__ == "__main__":
     unittest.main()
 
