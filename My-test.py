38e0ebffaf87a5ee0297b789d0b768df31f461e4
sofiasoler16 <s.soler@alumno.um.edu.ar>
2023-04-04 11:29:03 -0300

    my_tomanos.py

---
+++ My-test.py
@@ -0,0 +1,8 @@
+import unittest
+from my_romanos import decimal2roman
+class MyTest(unittest.TestCase):
+    def test_r(self):
+        self.assertEqual(decimal2roman(1), "I")
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ my_romanos.py
@@ -0,0 +1,8 @@
+def decimal2roman(number):
+    if number == 1:
+        return "I"
+    if number == 2:
+        return "II"
+
+if __name__ == "__main__":
+    print(decimal2roman(2))
